9ba2b4d4180628306458d8cfe3d35b1e6f810480
DmitriyALEX <doiroffical@gmail.com>
2025-11-27 13:34:19 +0200

    feature: added coordinates to first render and popup

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,17 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./style.css";
 import { useLocation } from "react-router-dom";
+import useFetchData from "../../hooks/useFetchData";
 
 const Map = () => {
+  const fetchedData = useFetchData();
   const mapRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const params = new URLSearchParams(location.search);
+
+  const [isOpenPopup, setIsOpenPopup] = useState<boolean>(false);
 
   const x = Number(params.get("x") || 0);
   const y = Number(params.get("y") || 0);
 
   // const xPercent = (x / 1800) * 100; // X → left
   // const yPercent = (y / 1800) * 100; // Y → top
+
+  //first render
+  useEffect(() => {
+    if (!fetchedData || fetchedData.length === 0) return;
+    const container = mapRef.current;
+    const marker = container?.querySelector<HTMLDivElement>(".marker-container");
+    if (!container || !marker) return;
+
+    marker.style.left = `${fetchedData[0].coordinates.x}px`;
+    marker.style.top = `${fetchedData[0].coordinates.y}px`;
+  }, [fetchedData]);
 
   useEffect(() => {
     const container = mapRef.current;
@@ -31,6 +46,9 @@ const Map = () => {
         left: realX - container.clientWidth / 2,
         behavior: "smooth",
       });
+
+      //close popup
+      setIsOpenPopup(false);
     };
 
     if (img.complete) {
@@ -46,14 +64,20 @@ const Map = () => {
       <img src='/map.png' alt='map' className='map' />
 
       {/* marker */}
-      <div
-        className='marker-container'
-        // style={{
-        //   top: `${yPercent}%`,
-        //   left: `${xPercent}%`,
-        // }}
-      >
-        <div className='marker-point'></div>
+      <div className='marker-container'>
+        <button className='marker-point' onClick={() => setIsOpenPopup(!isOpenPopup)}></button>
+        {/* popup */}
+        {isOpenPopup && (
+          <div className='popup'>
+            <div className='close-container'>
+              <button className='close-btn' onClick={() => setIsOpenPopup(false)}>
+                <img src='/public/close-icon.svg' className='close-icon' />
+              </button>
+            </div>
+
+            <div className='popup-content'>info</div>
+          </div>
+        )}
       </div>
     </div>
   );
